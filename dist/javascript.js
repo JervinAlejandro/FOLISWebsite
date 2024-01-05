6d40678c068e0f3ca021903404ea23aec390b813
Jervin <Jervin.Alejandro@hotmail.com>
2024-01-05 12:29:58 +0800

    Default

--- dist/javascript.js
+++
@@ -1,9 +0,0 @@
-function Menu(element) {
-    let menuDiv = document.getElementById("navbar-sticky");
-    
-    if(menuDiv.classList.contains("hidden")) {
-        menuDiv.classList.remove("hidden");
-    } else {
-        menuDiv.classList.add("hidden");
-    }
-}
