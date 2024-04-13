16ea4608f40ee89fced04ed49bb959cd71726a3b
Jervin <Jervin.Alejandro@hotmail.com>
2024-04-14 00:02:03 +0800

    Fix alignments of nav, contents and footer To better accomodate to smaller devices

--- dist/javascript.js
+++ dist/javascript.js
@@ -1,21 +1,52 @@
 function Menu(element) {
-    let menuDiv = document.getElementById("navbar-sticky");
-  
-    if (menuDiv.classList.contains("hidden")) {
-      menuDiv.classList.remove("hidden");
-    } else {
-      menuDiv.classList.add("hidden");
+  let menuDiv = document.getElementById("navbar-sticky");
+
+  if (menuDiv.classList.contains("hidden")) {
+    menuDiv.classList.remove("hidden");
+  } else {
+    menuDiv.classList.add("hidden");
+  }
+}
+
+function playVideo() {
+  var video = document.getElementById("videoPlayer");
+  var thumbnail = document.getElementById("videoThumbnail");
+  var playButton = document.getElementById("playButton");
+
+  thumbnail.style.display = "none";
+  playButton.style.display = "none";
+  video.style.display = "block";
+
+  video.play();
+}
+
+function addNavBg(element, yCoordination = sticky) {
+  if (window.scrollY > yCoordination) {
+    element.classList.add("bg-white");
+  } else {
+    element.classList.remove("bg-white");
+  }
+}
+
+function changeTextColour(element, yCoordination = sticky) {
+  if (window.scrollY > yCoordination) {
+    for (let i = 0; i < element.length; i++) {
+      element[i].classList.add("text-black");
+    }
+  } else {
+    for (let i = 0; i < element.length; i++) {
+      element[i].classList.remove("text-black");
     }
   }
-  
-  function playVideo() {
-    var video = document.getElementById("videoPlayer");
-    var thumbnail = document.getElementById("videoThumbnail");
-    var playButton = document.getElementById("playButton");
-  
-    thumbnail.style.display = "none";
-    playButton.style.display = "none";
-    video.style.display = "block";
-  
-    video.play();
-  }
+}
+
+window.onscroll = () => {
+  addNavBg(nav);
+  changeTextColour(navLinks);
+};
+
+let nav = document.getElementById("test");
+let sticky = nav.offsetTop;
+
+let list = document.getElementById("nav-links");
+let navLinks = list.getElementsByTagName("a");
